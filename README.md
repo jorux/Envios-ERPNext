8d23d5880ec8dc48132fe74a073b4ad0b6dabcfa
Rodrigo C <mistermaster0@gmail.com>
2022-03-20 22:04:34 +0000

    Añadido botón en sales invoice

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Simple app to create a doctype linked to sales invoice in ERPNext
 
 #### License
 
-MIT
+MIT# Envios-ERPNext
---
+++ envios_erpnext/envios_erpnext/doctype/envio/envio.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Jorux and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Envio', {
+	// refresh: function(frm) {
+
+	// }
+});
