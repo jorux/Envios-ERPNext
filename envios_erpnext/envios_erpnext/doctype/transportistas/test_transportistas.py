ec20882459ac95a85f240619db9a56a92657f2dc
Rodrigo C <mistermaster0@gmail.com>
2022-04-26 19:53:10 +0000

    Version 1.1, added transportistas

---
+++ envios_erpnext/envios_erpnext/doctype/transportistas/test_transportistas.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Jorux and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTransportistas(FrappeTestCase):
+	pass
---
+++ envios_erpnext/envios_erpnext/doctype/transportistas/transportistas.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Jorux and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Transportistas', {
+	// refresh: function(frm) {
+
+	// }
+});
